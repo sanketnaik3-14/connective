54fa51e9c70768b7811e4de35ba2853ef9e912fe
Sanket Naik <sanketnaik@Sankets-MacBook-Pro.local>
2025-08-11 15:24:24 +0530

    new file

--- lib/providers.dart
+++ lib/providers.dart
@@ -3,17 +3,18 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firestore_service.dart'; // Import the service we just created
 
-// Create an instance of our service that the provider can use.
+// This first provider creates a single instance of our FirestoreService.
+// This is a best practice that makes our service easy to access and test.
 final firestoreServiceProvider = Provider<FirestoreService>((ref) {
   return FirestoreService();
 });
 
-// This is our FutureProvider.
-// It uses ".family" because we need to pass it a parameter (the moduleId).
+// This is our FutureProvider. Its only job is to fetch the questions.
+// It uses ".family" because we need to pass it a parameter (the moduleId, e.g., 'module1').
 final quizQuestionsProvider =
     FutureProvider.family<List<Question>, String>((ref, moduleId) {
-  // It watches our service provider...
+  // It "watches" the service provider to get the instance...
   final firestoreService = ref.watch(firestoreServiceProvider);
-  // ...and calls the function to get the questions.
+  // ...and then calls the function to get the questions for the specified module.
   return firestoreService.getQuestionsForModule(moduleId);
 });
